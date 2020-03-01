a37f707c988bfc9eafb17b5cb03486681ea5a4a2
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-03-01 23:10:06 +0700

    sangat commit

---
+++ class.py
@@ -0,0 +1,10 @@
+class mahasiswa():
+    nama = 'Dessy Puspita Sari'
+
+Mahasiswa1 = mahasiswa() #Instance = Class
+Mahasiswa2 = mahasiswa()
+
+Mahasiswa1.nama = 'Rizky Khapidsyah'
+
+print(Mahasiswa1.nama)
+print(Mahasiswa2.nama)
